e4e7fec9102f7d088b0acc2b55eb3837728b1077
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-03-12 14:43:33 -0500

    Starts using ggigraph

--- inst/app1/app.R
+++ inst/app1/app.R
@@ -1,17 +1,16 @@
 library(shiny)
+library(ggiraph)
 
 source("functions.R")
 
 ui <- basicPage(
-  plotOutput("distPlot", height = "800px")
+  girafeOutput("distPlot", height = "800px")
 )
 
 server <- function(input, output) {
-
   autoInvalidate <- reactiveTimer(10000)
 
-  output$distPlot <- renderPlot({
-
+  output$distPlot <- renderGirafe({
     autoInvalidate()
 
     results <- get_responses(
@@ -19,14 +18,39 @@ server <- function(input, output) {
       "Form Responses 2"
     )
 
-    results %>%
+
+
+
+    p1 <- results |>
       ggplot() +
-      geom_col(aes(x = response, y = total, fill = response)) +
-      coord_flip() +
-      facet_grid(question ~ .) +
-      labs(x = "", y  = "")
+      geom_bar_interactive(
+        aes(
+          x = total,
+          y = response,
+          tooltip = total,
+          fill = response
+        ),
+        stat = "identity"
+      ) +
+      facet_wrap(vars(question), ncol = 1, strip.position = "right") +
+      labs(x = "", y = "") +
+      #theme_minimal() +
+      theme(
+        legend.position = "none",
+        axis.text.y = element_text(size = 5),
+        axis.text.x = element_text(size = 5),
+        strip.text.y = element_text(angle = 0),
+        strip.text = element_text(size = 6),
+        #plot.background = element_rect(fill = "#ddd"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_line("#ddd"),
+        panel.background = element_rect("white", colour = "#ddd")
+      )
 
-
+    girafe(
+      ggobj = p1,
+      options = list(opts_sizing(rescale = TRUE))
+    )
   })
 }
 
